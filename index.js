7754a3f8a67e3efd7921660a1c57b33f58e0f405
PoornaSri383 <poornasrinilla3002@gmail.com>
2022-12-17 21:44:23 +0530

    ToDo List

--- index.js
+++ index.js
@@ -1,56 +1,94 @@
-// callbacks
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
 
-// const fetchUserDetails = (userID, callback) => {
-//   console.log("fetching user details");
-//   setTimeout(() => {
-//     callback("https://image.com/${userID}");
-//   }, 500);
-// };
+  const overdue = () => {
+    function isOverDue(item) {
+      return item.dueDate < today && item.completed == false;
+    }
 
-// const downloadImage = (imageURL, callback) => {
-//     console.log("Downloading image");
-//     setTimeout(() => {
-//         callback("Image data for ${imageURL}");
-//     }, 500);
-//   };
+    return all.filter(isOverDue);
+  };
 
-//   const render = (image) => {
-//     console.log("render image");
-//   }
+  const dueToday = () => {
+    function isDueToday(item) {
+      return item.dueDate == today;
+    }
 
-//   fetchUserDetails("poorna", (imageURL) => {
-//     downloadImage(imageURL, (ImageData) => {
-//         render(ImageData);
-//     })
-//   })
+    return all.filter(isDueToday);
+  };
 
-//Promises
+  const dueLater = () => {
+    function isDueLater(item) {
+      return item.dueDate > today;
+    }
 
-const fetchUserDetails = (userID) => {
-  console.log("fetching user details");
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("https://image.com/${userID}");
-    }, 500);
-  });
+    return all.filter(isDueLater);
+  };
+
+  const toDisplayableList = (list) => {
+    var displayableList = list
+      .map((task) =>
+         `${task.completed ? "[x]" : "[ ]"} ${task.title} ${
+            task.dueDate !== today ? task.dueDate : " "
+          }`
+      )
+      .join("\n");
+
+    return displayableList;
+  };
+
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
 };
 
-const downloadImage = (imageURL) => {
-  console.log("Downloading image");
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Image data for ${imageURL}");
-    }, 500);
-  });
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+const todos = todoList();
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
 };
 
-const render = (image) => {
-    console.log("render image");
-  }
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
 
-  fetchUserDetails("poorna")
-  .then((imageURL) => downloadImage(imageURL))
-  .then((ImageData) => render(ImageData))
-  .catch((error) => {
-    console.log(error);
-  })
+todos.add({ title: "Submit assignment", dueDate: yesterday, completed: false });
+todos.add({ title: "Pay rent", dueDate: today, completed: true });
+todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
+todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
+todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
+
+console.log("My Todo-list\n\n");
+
+console.log("Overdue");
+var overdues = todos.overdue();
+var formattedOverdues = todos.toDisplayableList(overdues);
+console.log(formattedOverdues);
+console.log("\n\n");
+
+console.log("Due Today");
+let itemsDueToday = todos.dueToday();
+let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
+console.log(formattedItemsDueToday);
+console.log("\n\n");
+
+console.log("Due Later");
+let itemsDueLater = todos.dueLater();
+let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
+console.log(formattedItemsDueLater);
+console.log("\n\n");
+
+
