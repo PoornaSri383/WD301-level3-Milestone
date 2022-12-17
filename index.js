7105cd31069b62ad689465d8c5966eb423dc464f
PoornaSri383 <poornasrinilla3002@gmail.com>
2022-12-17 19:23:34 +0530

    Synchronous and Asynchronous functions

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ setTimeout(() => {
   console.log("Hello 2");
 }, 100);
 console.log("Hello 3");
+
+//callstack without Asynchronous Functions
+
+const fName = () => console.log("Poorna");
+
+const lName = () => console.log("Sri");
+
+const printName = () => {
+  console.log("My name is[synchronous]: ");
+  fName();
+  lName();
+};
+
+printName();
+
+//callstack with Asynchronous Functions
+
+const printNames = () => {
+    console.log("My name is[Asynchronous]: ");
+    setTimeout(fName, 0);
+    lName();
+}
+
+printNames();
