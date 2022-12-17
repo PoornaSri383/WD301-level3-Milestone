2a6272cf3b91e9413cd669a100febb7c50997aff
PoornaSri383 <poornasrinilla3002@gmail.com>
2022-12-17 19:33:42 +0530

    Closures in JS

--- index.js
+++ index.js
@@ -23,3 +23,21 @@ const printNames = () => {
 }
 
 printNames();
+
+// Closures in JavaScript
+function generateGreetings(name) {
+    function spanish() {
+      console.log(`Hola ${name}!`);
+    }
+    function english() {
+      console.log(`Hello ${name}!`);
+    }
+    return {spanish, english};
+  };
+  
+  const name = 'John';
+  const greetings = generateGreetings(name);
+  console.log(typeof(greetings.spanish));
+  
+  greetings.spanish(); 
+  greetings.english(); 
