c9a39716094e592cea9454e23eef0adf74f3e50e
PoornaSri383 <poornasrinilla3002@gmail.com>
2022-12-17 19:26:42 +0530

    Synchronous and Asynchronous functions

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-//  all the asynchronous functions will be executed only after the execution of Synchronous functions
-console.log("Hello 1");
 
-setTimeout(() => {
-  console.log("Hello 2");
-}, 100);
-console.log("Hello 3");
 
 //callstack without Asynchronous Functions
 
