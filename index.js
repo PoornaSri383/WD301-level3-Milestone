a679cf92a25e01af52eb2f134d31137a68a1e03d
PoornaSri383 <poornasrinilla3002@gmail.com>
2022-12-17 20:08:54 +0530

    promises

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-
-
 // callbacks
 
-const fetchUserDetails = (userID, callback) => {
+// const fetchUserDetails = (userID, callback) => {
+//   console.log("fetching user details");
+//   setTimeout(() => {
+//     callback("https://image.com/${userID}");
+//   }, 500);
+// };
+
+// const downloadImage = (imageURL, callback) => {
+//     console.log("Downloading image");
+//     setTimeout(() => {
+//         callback("Image data for ${imageURL}");
+//     }, 500);
+//   };
+
+//   const render = (image) => {
+//     console.log("render image");
+//   }
+
+//   fetchUserDetails("poorna", (imageURL) => {
+//     downloadImage(imageURL, (ImageData) => {
+//         render(ImageData);
+//     })
+//   })
+
+//Promises
+
+const fetchUserDetails = (userID) => {
   console.log("fetching user details");
-  setTimeout(() => {
-    callback("https://image.com/${userID}");
-  }, 500);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("https://image.com/${userID}");
+    }, 500);
+  });
 };
 
-const downloadImage = (imageURL, callback) => {
-    console.log("Downloading image");
+const downloadImage = (imageURL) => {
+  console.log("Downloading image");
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        callback("Image data for ${imageURL}");
+      resolve("Image data for ${imageURL}");
     }, 500);
-  };
+  });
+};
 
-  const render = (image) => {
+const render = (image) => {
     console.log("render image");
   }
 
-  fetchUserDetails("poorna", (imageURL) => {
-    downloadImage(imageURL, (ImageData) => {
-        render(ImageData);
-    })
+  fetchUserDetails("poorna")
+  .then((imageURL) => downloadImage(imageURL))
+  .then((ImageData) => render(ImageData))
+  .catch((error) => {
+    console.log(error);
   })
